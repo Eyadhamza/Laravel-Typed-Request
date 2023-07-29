e57c5364acc4cf4e5c0d554ab9f5f4ed77a7523c
Eyad Hamza <Eyadhamza000@gmail.com>
2023-07-30 00:39:06 +0300

    Typed Class

---
+++ src/TypedFormRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace PiSpace\LaravelTypedRequest;
+
+use Illuminate\Foundation\Http\FormRequest;
+use ReflectionClass;
+use ReflectionProperty;
+
+class TypedFormRequest extends FormRequest
+{
+
+    public function rules(): array
+    {
+        $properties = collect((new ReflectionClass($this))->getProperties(ReflectionProperty::IS_READONLY));
+
+        return $properties->mapWithKeys(function (ReflectionProperty $property){
+            $rules = collect($property->getAttributes())->map(fn($attribute) => $attribute->newInstance()->getRule())->all();
+
+            $this->initializeTypedProperty($property);
+
+            return [
+                $property->getName() => $rules
+            ];
+        })->all();
+    }
+
+    private function initializeTypedProperty(ReflectionProperty $property): void
+    {
+        $property->setValue($this, $this->input($property->getName()));
+    }
+
+}
